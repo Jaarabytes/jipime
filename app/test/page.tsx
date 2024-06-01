07bd9a38207cbdedecda0af395b2f41ca85925d7
Jaarabytes <xavierandole78@gmail.com>
2024-06-01 13:07:10 +0300

    feat: add timer component
    add timer component and it works perfectly

---
+++ app/test/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import Timer from "@/app/ui/Timer"
+
+export default function Test() {
+    return (
+        // Add 20 minutes timer
+        <>
+        <div>
+            <Timer />
+        </div>
+        </>
+    )
+}

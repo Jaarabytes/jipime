cbe1effe89ee39fcf3ea96ba5436188a95647647
Jaarabytes <xavierandole78@gmail.com>
2024-06-01 15:30:05 +0300

    feat: initialize `Patterns` component
    a component that stores patterns for the IQ app

---
+++ app/ui/test/Patterns.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Patterns () {
+    return (
+        <>
+            {/* This is the patterns part of the iq test. show patterns similar to mensa's iq test */}
+        </>
+    )
+}

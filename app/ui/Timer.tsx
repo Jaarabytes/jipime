04317eda35953d5d30b7d1945d9a28b4f730f2b1
Jaarabytes <xavierandole78@gmail.com>
2024-06-01 13:06:38 +0300

    feat: timer component for test countdown
    20 minutes are just about enough

---
+++ app/ui/Timer.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+
+export default function Timer () {
+    // Timer is 20 minutes, counts down from there
+    const [remainingTime, setRemainingTime] = useState(20 * 60);
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            if (remainingTime > 0) {
+                // reducing time by the second
+                setRemainingTime(prevTime => prevTime - 1);
+            }
+            else {
+                clearInterval(intervalId);
+            }
+        }, 1000) //update each second
+        return () => clearInterval(intervalId);
+    }, [])
+    const minutes = Math.floor( remainingTime/ 60 );
+    const seconds = remainingTime % 60;
+    return (
+        <>
+            <div>{minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, "0")}</div>
+        </>
+    )
+}

a220d30fd185eef337c698f203b297f0bb689cf8
Jaarabytes <xavierandole78@gmail.com>
2024-06-01 16:43:17 +0300

    feat: add more responsiveness
    add tailwindcss classes for more responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
+import Age from "@/app/ui/home/Age"
+
 export default function HomePage() {
   return (
     <>
-      <h1 className="text-3xl font-bold">IQ test</h1>
-      <p>Made by <a href="https://jaarabytes.vercel.app" className="text-red-400">Jaarabytes</a></p>
-      <hr />
-      <p>This test just tries to boost your confidence levels by guessing your IQ levels. It provides an estimate between the values 80-130,
-        where 100 is the population average. This is not real, please for real IQ tests <a href="/real" 
-        className="text-red-400 underline">click here</a>
-      </p>
-      <p>
-        This test has some simple questions which you can try to complete before 20 minutes are over. You do get points for finishing the test earlier. 
-        You are also not penalised when you answer wrongly, so Harvey Dent your way through it.
-      </p>
-      <p>Get rid of all the noise surrounding you so that you won&apos;t argue that &apos;I'm bright, the noise was distracting me!&apos;</p>
+      <div className="px-5 text-center">
+        <h1 className="text-[60px] font-bold">IQ test</h1>
+        <p className="text-2xl py-5">Made by <a href="https://jaarabytes.vercel.app" className="text-red-400">Jaarabytes</a></p>
+        <hr />
+        <div className="px-60 my-5">
+          <p className="text-lg my-5">This test just tries to boost your confidence levels by guessing your IQ levels. It provides an estimate between the values 80-130,
+            where 100 is the population average. This is not real, please for real IQ tests <a href="/real" 
+            className="text-red-400 underline">click here</a>
+          </p>
+          <p className="text-lg">
+            This test has some simple questions which you can try to complete before 20 minutes are over. You do get points for finishing the test earlier. 
+            You are also not penalised when you answer wrongly, so Harvey Dent your way through it.
+          </p>
+          <p className="text-lg my-5 text-lg">Create a conducive environment around you by getting rid of all the noise surrounding you.</p>
+        </div>
+        <Age />
+      </div>
     </>
   )
 }

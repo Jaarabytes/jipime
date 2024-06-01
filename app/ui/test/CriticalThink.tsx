c06f28e30b0ab0e770a2c99c0dd83baafac044a0
Jaarabytes <xavierandole78@gmail.com>
2024-06-01 15:37:54 +0300

    feat: add `CriticalThink` component
    it contains the last section of the tests which is just to make you fail the test

---
+++ app/ui/test/CriticalThink.tsx
@@ -0,0 +1,10 @@
+
+
+export default function CriticalThink () {
+    // Series of questions that is supposed to make you fail my test.
+    // yes, I am against you
+    return (
+        <>
+        </>
+    )
+}

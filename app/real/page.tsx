d6fafcd3d03c5df30bda7aa43e1a0c0728cb2a6f
Jaarabytes <xavierandole78@gmail.com>
2024-06-01 13:08:31 +0300

    feat: add more iq tests
    add relevant iq tests for other people

--- app/real/page.tsx
+++ app/real/page.tsx
@@ -1,5 +1,9 @@
 export default function RealPage() {
-    const list = [{name: "IQTest.com", link: "https://iqtest.com"}, {name: "Mensa Norway", link:"https://test.mensa.no"}]
+    const list = [{name: "IQTest.com", link: "https://iqtest.com"}, {name: "Mensa Norway", link:"https://test.mensa.no"},
+        {name: "Free IQ test", link:"https://freeiqtest.info"}, {name:"Bright org", link: "brght.org"}, 
+        {name: "My IQ tested", link:"https://myiqtested.com"}, {name: "Mensa Finland", link:"https://mensa.fi/test"},
+        {name: "international Iq test", link: "https://international-iq-test.com"}, {name: "Free IQ test (better)", link: "https://free-iqtest.net"}
+    ]
 
     return (
         <>

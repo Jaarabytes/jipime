ca121efe5021b9aa23d246bc78c3f6dab21fcd1f
Jaarabytes <xavierandole78@gmail.com>
2024-06-04 20:13:24 +0300

    fix: fix `brght.org` link error
    add `https://` to fix link routing error

--- app/real/page.tsx
+++ app/real/page.tsx
@@ -1,13 +1,13 @@
 export default function RealPage() {
     const list = [{name: "IQTest.com", link: "https://iqtest.com"}, {name: "Mensa Norway", link:"https://test.mensa.no"},
-        {name: "Free IQ test", link:"https://freeiqtest.info"}, {name:"Bright org", link: "brght.org"}, 
+        {name: "Free IQ test", link:"https://freeiqtest.info"}, {name:"Bright org", link: "https://brght.org"}, 
         {name: "My IQ tested", link:"https://myiqtested.com"}, {name: "Mensa Finland", link:"https://mensa.fi/test"},
         {name: "international Iq test", link: "https://international-iq-test.com"}, {name: "Free IQ test (better)", link: "https://free-iqtest.net"}
     ]
 
     return (
         <>
-            <div>
+            <div className="px-5 my-5">
                 <p>Here is a list of some good IQ tests (They are not better than <a href="/" 
                 className="text-red-400 underline">jipime</a>, however they inspired me)</p>
                 <div className="px-5">

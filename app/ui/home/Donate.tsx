9c629896031dd60c4f47e51fbb49bfa567dd1ce4
Jaarabytes <xavierandole78@gmail.com>
2024-06-04 16:41:56 +0300

    feat: fund me
    money upvote feature, now i have to go full stack

---
+++ app/ui/home/Donate.tsx
@@ -0,0 +1,14 @@
+
+
+
+export default function Upvote() {
+
+    return (
+        <>
+            <div className="my-5">
+                <p className="my-3">You are high IQ bro, now fund me</p>
+                <button className="bg-green-900 px-3 py-3 rounded-lg hover:bg-green-700">Money upvote</button>
+            </div>
+        </>
+    )
+}

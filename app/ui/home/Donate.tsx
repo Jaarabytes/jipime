1d6d0c7b5b15e47fe1ceb69e0c086d4e74a79776
Jaarabytes <xavierandole78@gmail.com>
2024-06-05 05:39:05 +0300

    feat: money upvote button for donation
    yeah, i need money

--- app/ui/home/Donate.tsx
+++ app/ui/home/Donate.tsx
@@ -1,13 +1,16 @@
-
-
-
+import { MdKeyboardArrowUp } from "react-icons/md"
+import Link from "next/link"
 export default function Upvote() {
-
+// Link to payments pages, learn coinbase (metamask), stripe, daraja
     return (
         <>
+        <hr className="my-5"/>
             <div className="my-5">
                 <p className="my-3">You are high IQ bro, now fund me</p>
-                <button className="bg-green-900 px-3 py-3 rounded-lg hover:bg-green-700">Money upvote</button>
+                <Link href={`/lipa`}>
+                    <button className="bg-green-900 text-white px-3 py-3 rounded-lg 
+                    hover:bg-green-700"><MdKeyboardArrowUp className="inline"/> Money upvote <MdKeyboardArrowUp className="inline"/></button>
+                </Link>
             </div>
         </>
     )

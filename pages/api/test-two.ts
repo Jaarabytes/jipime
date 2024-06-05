ffc031b4d054ff7314ec799b1a03df906caa39b3
Jaarabytes <xavierandole78@gmail.com>
2024-06-05 18:15:17 +0300

    feat: calculate IQ for test-two
    add basic structure to it

---
+++ pages/api/test-two.ts
@@ -0,0 +1,5 @@
+import { NextApiRequest, NextApiResponse } from "next";
+// computation for test two of the exams
+export default async function handler (req: NextApiRequest, res:NextApiResponse ){
+    
+}

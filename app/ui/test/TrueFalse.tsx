174f7ed8dc4dcbc5b646e89851601871fdf7fc39
Jaarabytes <xavierandole78@gmail.com>
2024-06-05 19:45:22 +0300

    feat: add some big changes
    add functionality for form submission and acceptance on the api endpoint

--- app/ui/test/TrueFalse.tsx
+++ app/ui/test/TrueFalse.tsx
@@ -1,6 +1,7 @@
-
-
+import { MdOutlineKeyboardArrowRight } from "react-icons/md"
+import { useState } from "react"
 export default function TrueFalse () {
+
     // This component holds all the true or false questions of the iq test. (part 1)
     const questions = [
         // answer: 77 + 33 = 110
@@ -24,20 +25,72 @@ export default function TrueFalse () {
         // answer: mirrors are opposite
         {question: "If I look into a mirror and touch my left ear with my right hand, the guy in the mirror is touching his right ear with his left hand", answer: true},
     ]
+
+    const [ formData, setFormData ] = useState({
+        'question-1': '',
+        'question-2': '',
+        'question-3': '',
+        'question-4': '',
+        'question-5': '',
+        'question-6': '',
+        'question-7': '',
+        'question-8': '',
+        'question-9': '',
+        'question-10': ''
+      });
+    const [ response, setResponse ] = useState(null);
+
+    // initialize all questions to false since useState() is stupid. I'm switching to svelte
+    // const initialFormData = questions.reduce((acc, question, index) => {
+    //     acc[`question-${index}`] = 'False';
+    //     return acc;
+    // }, {})
+
+    const handleChange = (e: any) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({...prevData, [name]: value }))
+   }
+   
+   const handleSubmit = async (e: any) => {
+       e.preventDefault();
+       try {
+           const res = await fetch ("/api/test-one", {
+               method: "POST",
+               headers: { "Application-Type": "JSON"} ,
+               body: JSON.stringify(formData)
+           })
+           const data = await res.json();
+           setResponse(data);
+           if ( res.ok ) {
+               console.log("Successfully submitted test-one")
+           }
+           else{
+               console.log("Error when submitting test-one")
+           }
+       }
+       catch (err) {
+           console.error("error when submitting test-one", err)
+       }
+   } 
+    // add a next button that submits all answers at once to calculator
+    // have a function in calculator that checks and keeps score
     return (
         <>
             <div className="m-5">
-                {questions.map((element, index) => (
-                    <div key={index} className="my-4">
-                        <form>
-                        <p>{index + 1}. <span className="mx-4">{element.question}</span></p>
-                                <div className="mx-5">
-                                    <label className="block"><input type="radio" name="trueorfalse" className="mx-2" value="True"></input>True</label>
-                                    <label className="block"><input type="radio" name="trueorfalse" className="mx-2" value="False"></input>False</label>
-                                </div>
-                        </form>
-                    </div>
-                ))}
+                <form id="true-false" onSubmit={handleSubmit}>
+                    {questions.map((element, index) => (
+                        <div key={index} className="my-4">
+                            <p>{index + 1}. <span className="mx-4">{element.question}</span></p>
+                                    <div className="mx-5">
+                                        <label className="block"><input type="radio" name={`question-${index + 1}`} className="mx-2" value={`True`} onChange={handleChange}></input>True</label>
+                                        <label className="block"><input type="radio" name={`question-${index + 1}`} className="mx-2" value={`False`} onChange={handleChange}></input>False</label>
+                                    </div>
+                        </div>
+                    ))}
+                    <button className="text-center m-5 text-xl bg-blue-900 text-white rounded-lg px-5 py-3 hover:bg-blue-700"
+                    type="submit"
+                    >Next <MdOutlineKeyboardArrowRight className="w-6 inline" /></button>
+                </form>
             </div>
         </>
     )

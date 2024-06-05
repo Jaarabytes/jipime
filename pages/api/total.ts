9fc0a1577b76be72718105a5180f51c5f33fbf75
Jaarabytes <xavierandole78@gmail.com>
2024-06-05 18:14:52 +0300

    feat: calculate total IQ and give stats on it
    add basic structure to it

---
+++ pages/api/total.ts
@@ -0,0 +1,5 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler (req: NextApiRequest, res:NextApiResponse ){
+//  here the total and final computation is done   
+}

32b93271972aae6fb0db61a5fc58d4826e6fecf0
Jaarabytes <xavierandole78@gmail.com>
2024-06-07 13:41:46 +0300

    feat: add api route to receive complaints
    implement basic structure of ts api route

---
+++ pages/api/complain.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from "next";
+// receives complains and forwards them to my stupid mail
+
+export default async function handler (req: NextApiRequest, res:NextApiResponse ) {
+
+}

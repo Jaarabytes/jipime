e322e94e6992fae2825f6ebb737d59f04ce553ac
Jaarabytes <xavierandole78@gmail.com>
2024-06-07 14:56:15 +0300

    feat: api endpoint for mailgun
    todo: enable mailgun account

--- pages/api/complain.ts
+++ pages/api/complain.ts
@@ -1,6 +1,32 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import mailgun from "mailgun-js"
 // receives complains and forwards them to my stupid mail
 
-export default async function handler (req: NextApiRequest, res:NextApiResponse ) {
+// declare mailgun variables
+const mg = mailgun({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: "mydomain.com"
+})
 
+
+export default async function handler (req: NextApiRequest, res:NextApiResponse ) {
+    if (req.method == "POST") {
+        const { complaint } = req.body;
+        try {
+            await mg.messages().send({
+                from: "Jipime user",
+                to: "xh3rking96@gmail.com",
+                subject: "Jipime complaint",
+                text: complaint,
+            });
+            res.status(200).json({message: "Email sent successfully"})
+        }
+        catch ( err ) {
+            res.status(500).json({error: "Failed to send mail"});
+            console.error("Error when sending mail", err)
+        }
+    }
+    else {
+        res.status(405).json({error: "Method NOT ALLOWED"})
+    }
 }

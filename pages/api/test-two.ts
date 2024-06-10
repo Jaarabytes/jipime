8e26fcd57255fcf81f4442e87c80b7f845e2848d
Jaarabytes <xavierandole78@gmail.com>
2024-06-10 17:15:41 +0300

    fix: implement working api
    add api endpoint for test 2

--- pages/api/test-two.ts
+++ pages/api/test-two.ts
@@ -1,17 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
 // computation for test two of the exams
-// Patterns is the dumbest part
-// Draw the canva patterns + designs for the images
-// Please change the below code
 export default async function handler (req: NextApiRequest, res:NextApiResponse ){
     if ( req.method == "POST" ) {
-        let testOneScore = 0;
+        let testTwoScore = 0;
         console.log("The request body is", req.body);
         const userChoices = JSON.parse(req.body);
-        // Simple testOneScore, delete later
+        // Simple testTwoScore, delete later
         console.log("Values equals", userChoices);
         // create an array of answers and countercheck with values provided by user
-        const answers = ["False", "True", "True", "False", "False", "False", "True", "True", "False", "True"];
+        const answers = ["True", "False", "False", "False", "True", "False", "True", "False", "True"];
         // initialize types
         type userChoices = {[key: string]: string};
         type answers = string[]
@@ -21,13 +18,15 @@ export default async function handler (req: NextApiRequest, res:NextApiResponse 
             // console.log("Mapped into, ",values);
             for (let i = 0; i < values.length; i++ ){
                 if (values[i] == answers[i]) {
-                    testOneScore++
+                    testTwoScore++
                 }
             }
-            console.log(testOneScore);
-            return testOneScore;
+            console.log(testTwoScore);
+            return testTwoScore;
         }
         checkValidity(userChoices, answers);
+        const response = { testTwoScore }
+        return res.status(200).json({response});
     }
     else {
         res.setHeader('Allow', ["POST"]);

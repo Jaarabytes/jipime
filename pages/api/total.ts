8a4270c94bd359f4a8809dc70e3efc9395883684
Jaarabytes <xavierandole78@gmail.com>
2024-06-10 17:15:07 +0300

    fix: fix `json()` issue
    fix the json function

--- pages/api/total.ts
+++ pages/api/total.ts
@@ -2,4 +2,33 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler (req: NextApiRequest, res:NextApiResponse ){
 //  here the total and final computation is done   
+
+// fetch initial IQ and test-IQ's
+// also, factor in time
+    fetchIQScores();
+    // deal with the timer component
+}
+
+async function fetchIQScores () {
+    try {
+        const [ initialIQ, score1, score2, score3 ] = await Promise.all([
+            // fetch initial IQ
+            fetch('/api/age').then(response => response.json()),
+            // fetch score from test-one
+            fetch('/api/test-one').then(response => response.json()),
+            // fetch score from test-two
+            fetch('/api/test-two').then(response => response.json()),
+            // fetch score from test-three
+            fetch('/api/test-three').then(response => response.json())
+        ]);
+        console.log('Initial IQ:', initialIQ);
+        console.log('Test One Score:', score1);
+        console.log('Test Two Score:', score2);
+        console.log('Test Three Score:', score3);
+        
+        return { initialIQ, score1, score2, score3 };
+    }
+    catch ( err ) {
+        console.error("Error when fetching data from endpoints", err);
+    }
 }

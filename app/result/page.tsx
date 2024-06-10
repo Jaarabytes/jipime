aaff62a522eef0f2b1af1aaa9c7be26440db4880
Jaarabytes <xavierandole78@gmail.com>
2024-06-10 19:36:08 +0300

    feat: this should work
    remove `getStaticProps` since it doesn't work under /app

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,19 +1,23 @@
 import Upvote from "@/app/ui/home/Donate"
 
-export default function Result () {
-
-
+export default async function Result () {
+    const data = await getData();
     return (
         <>
             <div className="text-center" style={{minHeight: "100vh"}}>
                 {/* display IQ results here */}
                 {/* replace <this> with relevant values */}
-                <h2 className="font-bold text-xl">Your IQ was measured to be</h2>
-                {/* Display bell curve here */}
+                <h2 className="font-bold text-xl">Your IQ was measured to be { data }</h2>
                 <p>You IQ was measured to be this which is equivalent to the this percentile, with a standard deviation of this</p>
                 <p>Great test, yes? Give me money</p>
                 <Upvote />
             </div>
         </>
     )
+}
+
+export async function getData () {
+    const response = await fetch ("/api/total");
+    const data = await response.json();
+    return data;
 }

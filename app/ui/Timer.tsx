0d8974bd2c38b7b8dc2379d450aec60a448cc1ca
Jaarabytes <xavierandole78@gmail.com>
2024-06-11 14:43:46 +0300

    fix: initialized types and add useContext
    yeap, it's better now

--- app/ui/Timer.tsx
+++ app/ui/Timer.tsx
@@ -1,42 +1,37 @@
-import React, { useEffect, useState } from "react";
+'use client'
+import React, { useEffect, useState, createContext, useContext, ReactNode, Component } from "react";
 
-export default function Timer() {
-    const [time, setTime] = useState({ minutes: 0, seconds: 0 });
-    const [limitReached, setLimitReached] = useState(false);
+// define types
+interface TimerContextType {
+    currentTime : number,
+    setCurrentTime : React.Dispatch<React.SetStateAction<number>>
+}
 
+const TimerContext = createContext<TimerContextType>({
+    currentTime: 0,
+    setCurrentTime: () => {}
+})
+
+// define TimerProvider Component
+interface TimerProviderProps {
+    children: ReactNode;
+}
+
+export const TimerProvider: React.FC<TimerProviderProps> =  ({ children }) => {
+    const [ currentTime, setCurrentTime ] = useState(0);
+    // React is honestly just funny and stupid
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setTime((prevTime) => {
-                const { minutes, seconds } = prevTime;
-                if (seconds === 59) {
-                    if (minutes === 29) {
-                        setLimitReached(true);
-                        clearInterval(intervalId);
-                        return prevTime;
-                    }
-                    return { minutes: minutes + 1, seconds: 0 };
-                } else {
-                    return { ...prevTime, seconds: seconds + 1 };
-                }
-            });
-        }, 1000);
-
-        return () => clearInterval(intervalId);
-    }, []);
-
-    const handleCloseAlert = () => setLimitReached(false);
+            setCurrentTime((prevTime) => (prevTime + 1))
+        }, 1000)
+        return () => clearInterval(intervalId)
+    }, [])
 
     return (
-        <>
-            <div className="text-3xl m-5 text-center">
-                {time.minutes.toString().padStart(2, '0')}:{time.seconds.toString().padStart(2, "0")}
-            </div>
-            {limitReached && (
-                <div className="alert">
-                    Time limit reached!
-                    <button onClick={handleCloseAlert}>Close</button>
-                </div>
-            )}
-        </>
-    );
+        <TimerContext.Provider value={{ currentTime, setCurrentTime }}>
+            { children }
+        </TimerContext.Provider>
+    )
 }
+
+export const useTimer = () => useContext(TimerContext);

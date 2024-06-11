8565d709d5f8dae7c3bc138fd8bab8b1729e3068
Jaarabytes <xavierandole78@gmail.com>
2024-06-11 14:41:38 +0300

    fix: implement proper api routing
    add good http routing to allow data fetch

--- pages/api/total.ts
+++ pages/api/total.ts
@@ -7,19 +7,20 @@ export default async function handler (req: NextApiRequest, res:NextApiResponse 
 // also, factor in time
     fetchIQScores();
     // deal with the timer component
+    // return something, this is to be fixed
 }
 
 async function fetchIQScores () {
     try {
         const [ initialIQ, score1, score2, score3 ] = await Promise.all([
             // fetch initial IQ
-            fetch('/api/age').then(response => response.json()),
+            fetch('http://localhost:3000/api/age').then(response => response.json()),
             // fetch score from test-one
-            fetch('/api/test-one').then(response => response.json()),
+            fetch('http://localhost:3000/api/test-one').then(response => response.json()),
             // fetch score from test-two
-            fetch('/api/test-two').then(response => response.json()),
+            fetch('http://localhost:3000/api/test-two').then(response => response.json()),
             // fetch score from test-three
-            fetch('/api/test-three').then(response => response.json())
+            fetch('http://localhost:3000/api/test-three').then(response => response.json())
         ]);
         console.log('Initial IQ:', initialIQ);
         console.log('Test One Score:', score1);

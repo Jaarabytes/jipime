e2b59665ef22f17fa625909148a365031c264e99
Jaarabytes <xavierandole78@gmail.com>
2024-06-11 14:55:57 +0300

    refactor: changes are being made darling
    changing the project structure

--- app/test-2/page.tsx
+++
@@ -1,9 +0,0 @@
-// just import patterns tsx file
-import Patterns from "@/app/ui/test/Patterns";
-import { useTimer } from "@/app/ui/Timer";
-
-export default function TestTwo () {
-    return (
-            <Patterns />
-    )
-}

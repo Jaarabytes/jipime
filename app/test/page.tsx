664586b9eef30438061ddd504292dae412451a8d
Jaarabytes <xavierandole78@gmail.com>
2024-06-11 18:14:15 +0300

    I'm stupid and smart honestly. Possible issue with setting null state with formdata

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -7,10 +7,28 @@ import { AppProps } from "next/app"
 import { useState } from "react"
 
 export default function Test({Component, pageProps}: AppProps) {
+    // I don't want to make this full-stack since i'm wasting resources so results may just be implemented here
+    const [ testOneData, setFormOneData ] = useState(null);
+    const [ testTwoData, setFormTwoData ] = useState(null);
+    const [ testThreeData, setFormThreeData ] = useState(null);
     const [ currentForm, setCurrentForm ] = useState(1);
+    const [ totalScore, setTotalScore ] = useState<number | null>(null);
 
-    const handleNextForm = () => {
+    const handleNextForm = (data: any) => {
+        if ( currentForm === 1 ) setFormOneData(data);
+        if ( currentForm === 2 ) setFormTwoData(data);
+        if ( currentForm === 3 ) setFormThreeData(data);
         setCurrentForm(currentForm + 1);
+    }
+
+    const handleSubmitAllForms = async () => {
+        const response = await fetch ( "/api/result", {
+            method: "POST",
+            headers: {"Content-Type": "Application/json"},
+            body: JSON.stringify({testOne: testOneData, testTwo: testTwoData, testThree: testThreeData})
+        })
+        const result = await response.json();
+        setTotalScore(result.totalScore)
     }
 
     const renderForm = () => {
@@ -21,9 +39,12 @@ export default function Test({Component, pageProps}: AppProps) {
                 return <Patterns onNext={handleNextForm} />
             case 3:
                 // onNext() is not here since after this test the user is redirected to the results page
-                return <CriticalThink />
+                return <CriticalThink onNext={handleNextForm} />
+            case 4:
+                handleSubmitAllForms();
+                return <p>Calculating total score ...</p>
             default:
-                return <TrueFalse onNext={handleNextForm} />
+                return null;
         }
     }
     return (

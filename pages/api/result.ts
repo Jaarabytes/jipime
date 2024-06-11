f9c49148649c0a4dff843f1b5a35e2f8d75bb9d8
Jaarabytes <xavierandole78@gmail.com>
2024-06-11 18:13:45 +0300

    there is a serious problem here with POST requests, PLEASE FIX ME

--- pages/api/result.ts
+++ pages/api/result.ts
@@ -1,35 +1,28 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import checkValidity from "@/pages/api/checkValidity";
+const TestThreeAnswers = ["fleeting", "artist", "magnanimous", "sculpture", "True", "Box that has misfortunes and hope trapped inside",
+         "False", "Cultural trend", "Economy", "argue with the opposite"];
+const TestTwoAnswers = ["True", "False", "False", "False", "True", "False", "True", "False", "True"];
+const TestOneAnswers = ["False", "True", "True", "False", "False", "False", "True", "True", "False", "True"];
 
 export default async function handler (req: NextApiRequest, res:NextApiResponse ){
-//  here the total and final computation is done   
+    if ( req.body === "POST" ) {
+        const { test1, test2, test3 } = req.body;
 
-// fetch initial IQ and test-IQ's
-// also, factor in time
-    fetchIQScores();
-    // deal with the timer component
-    // return something, this is to be fixed
-}
+        if ( !test1 || !test2 || !test3 ) {
+            return res.status(400).json({ error: "All form data must be provided"})
+        }
 
-async function fetchIQScores () {
-    try {
-        const [ initialIQ, score1, score2, score3 ] = await Promise.all([
-            // fetch initial IQ
-            fetch('http://localhost:3000/api/age').then(response => response.json()),
-            // fetch score from test-one
-            fetch('http://localhost:3000/api/test-one').then(response => response.json()),
-            // fetch score from test-two
-            fetch('http://localhost:3000/api/test-two').then(response => response.json()),
-            // fetch score from test-three
-            fetch('http://localhost:3000/api/test-three').then(response => response.json())
-        ]);
-        console.log('Initial IQ:', initialIQ);
-        console.log('Test One Score:', score1);
-        console.log('Test Two Score:', score2);
-        console.log('Test Three Score:', score3);
-        
-        return { initialIQ, score1, score2, score3 };
+        const testOneScore = checkValidity( test1, TestOneAnswers )
+        const testTwoScore = checkValidity( test2, TestTwoAnswers )
+        const testThreeScore = checkValidity( test3, TestThreeAnswers )
+
+        const totalScore =  testOneScore + testTwoScore  + testThreeScore;
+        // add time factor and initialIQ (from age)
+        return res.status(200).json({ totalScore });
     }
-    catch ( err ) {
-        console.error("Error when fetching data from endpoints", err);
+    else {
+        res.setHeader('Allow', ["POST"]);
+        res.status(405).end(`Method ${req.method} not allowed`)
     }
 }

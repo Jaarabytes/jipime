203d9cdb5f7f2bec8584afd3b18476dd90b649b0
Jaarabytes <xavierandole78@gmail.com>
2024-06-12 20:44:26 +0300

    refactor: remove garbage
    remove extra `console.logs()` and other tests. I'm not using jest or mocha or chai yet

--- pages/api/age.ts
+++ pages/api/age.ts
@@ -1,23 +1,24 @@
 // Create another TS file to store chartjs and its intricacies
 import { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase, User } from "./connect";
+import { parseCookies } from "@/pages/api/cookieParser";
+import { Cookies } from "@/pages/api/cookieParser";
 // Here we calculate IQ buddy
 
 export default async function handler( req: NextApiRequest, res: NextApiResponse) {
     if ( req.method == "POST" ) {
-        // sanitize if statement since this api endpoint is used twice (age and criticalthink)
+        await connectToDatabase();
         let starterIQ = 100;
-        // The below if statement just does the age computation
-        const { selectedAge } = req.body;
+        const { selectedAge, userId } = req.body;
         console.log(req.body)
 
         if ( selectedAge ){
             // I don't give out free points
             starterIQ += 2;
         }
-
-        const response = { starterIQ };
-        console.log(response);
-        return res.status(200).json({response});
+        const newUser = new User({userId , starterIQ});
+        await newUser.save();
+        return res.status(200).json(newUser);
     }
     else {
         res.setHeader('Allow', ["POST"]);

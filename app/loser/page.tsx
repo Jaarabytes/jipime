393cea26816b1e311bc6632fe43b5de76370ebd6
Jaarabytes <xavierandole78@gmail.com>
2024-06-12 13:19:58 +0300

    refactor: responsive fo rmobile phones
    responsive for mobile phones and remove shitty useless react code

--- app/loser/page.tsx
+++ app/loser/page.tsx
@@ -6,7 +6,7 @@ export default function Loser () {
     return (
         <>
         {/* Make me responsive for mobile phones */}
-            <div className="text-center my-5">
+            <div className="text-center my-5" style={{minHeight: "100vh"}}>
                 <h1 className="font-bold text-3xl my-5">Thanks for participating!</h1>
                 
                 <form action="mailto:xavierandole@gmail.com" method="POST">
@@ -15,7 +15,7 @@ export default function Loser () {
                     <textarea placeholder="Submit them here and be short as possible" 
                     value={complaint}
                     onChange={(e) => setComplaint(e.target.value)}
-                    className="border-none w-96 h-72 text-black my-5"></textarea>
+                    className="border-none sm:w-96 w-80 h-72 sm:h-72 p-3 text-black my-5"></textarea>
                     <br />
                     <button className="text-white w-36 my-5 rounded-lg p-3 bg-red-700 hover:bg-red-900" type="submit" value="Submit">Submit</button>
                 </form>

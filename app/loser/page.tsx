0ac3e6b13132b157c30499707b1396b5b43410f4
Jaarabytes <xavierandole78@gmail.com>
2024-06-12 13:15:07 +0300

    refactor: mail sent directly
    mail is sent directly to my inbox

--- app/loser/page.tsx
+++ app/loser/page.tsx
@@ -22,10 +22,10 @@ export default function Loser () {
     return (
         <>
         {/* Make me responsive for mobile phones */}
-            <div className="text-center">
-                <h1 className="font-bold text-3xl">Thanks for participating!</h1>
+            <div className="text-center my-5">
+                <h1 className="font-bold text-3xl my-5">Thanks for participating!</h1>
                 
-                <form onSubmit={handleSubmit}>
+                <form action="mailto:xavierandole@gmail.com" method="POST">
                     <label className="mt-10 mb-3">Please submit your complaints below</label>
                     <br />
                     <textarea placeholder="Submit them here and be short as possible" 
@@ -33,7 +33,7 @@ export default function Loser () {
                     onChange={(e) => setComplaint(e.target.value)}
                     className="border-none w-96 h-72 text-black my-5"></textarea>
                     <br />
-                    <button className="text-white w-36 my-5 rounded-lg p-3 bg-red-700 hover:bg-red-900" type="submit">Submit</button>
+                    <button className="text-white w-36 my-5 rounded-lg p-3 bg-red-700 hover:bg-red-900" type="submit" value="Submit">Submit</button>
                 </form>
 
                 <p>If you are a dev, even better (fix it yourself)</p>

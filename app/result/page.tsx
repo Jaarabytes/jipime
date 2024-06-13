431fdf454dbf46a54dd46839490c5ade4d9a2f95
Jaarabytes <xavierandole78@gmail.com>
2024-06-13 08:20:53 +0300

    fix: it works
    display results and proper UI design. Now to fix the TimerProvider

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,23 +1,42 @@
+'use client'
 import Upvote from "@/app/ui/home/Donate"
+import { useState, useEffect } from "react"
+export default function Result () {
+    // soon to be the funniest component
+    const [ userResults , setUserResults ] = useState({iq: null, deviation: null, percentile: 0});
 
-export default async function Result () {
-    const data = await getData();
+    useEffect(() => {
+        const fetchUserResults = () => {
+            fetch ( "/api/total", {
+                method: "GET",
+            })
+            .then((response) => {
+                if ( response.ok ) {
+                    return response.json()
+                }else {
+                    throw new Error("Error fetching results!")
+                }
+            }).then((data) => {
+                console.log(data);
+                setUserResults({iq:data.iq, deviation: data.deviation, percentile: data.percentile});
+            }).catch((error) => {
+                alert(error.message);
+                console.error(error)
+            })
+            }
+        fetchUserResults();
+    }, [])
     return (
         <>
             <div className="text-center" style={{minHeight: "100vh"}}>
-                {/* display IQ results here */}
-                {/* replace <this> with relevant values */}
-                <h2 className="font-bold text-xl">Your IQ was measured to be { data }</h2>
-                <p>You IQ was measured to be this which is equivalent to the this percentile, with a standard deviation of this</p>
+                <h2 className="font-bold text-3xl my-5">User results</h2>
+                <h2 className="font-bold text-xl">Your IQ was measured to be {userResults.iq}</h2>
+                <p>You IQ was measured to be <b>{userResults.iq}</b> which is equivalent to the <b>{Math.round(userResults.percentile)}</b>th percentile, 
+                with a standard deviation of <b>{userResults.deviation}</b></p>
+                <p>In a room filled with 1000 people, you'd be position <b>{Math.round(100 - userResults.percentile) * 10}</b></p>
                 <p>Great test, yes? Give me money</p>
                 <Upvote />
             </div>
         </>
     )
-}
-
-export async function getData () {
-    const response = await fetch ("http://localhost:3000/api/total");
-    const data = await response.json();
-    return data;
 }

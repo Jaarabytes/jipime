52d1e87ec4d166cbbd2af4354911a6889070f4c2
Jaarabytes <xavierandole78@gmail.com>
2024-06-13 08:22:09 +0300

    feat: implement deviation and percentile calculation
    add various functions to calculate user deviation and percentile.

--- pages/api/total.ts
+++ pages/api/total.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { connectToDatabase, User } from "./connect";
 import { parseCookies, Cookies } from "./cookieParser";
+import { erf } from 'mathjs'
 export default async function handler (req: NextApiRequest, res:NextApiResponse ){
 // try to implement bonus points if user completes before expected time
 // fix: user 404 when delivering results
@@ -10,13 +11,44 @@ export default async function handler (req: NextApiRequest, res:NextApiResponse 
             // get cookies/userId
             const cookies: Cookies = parseCookies(req.headers);
             const userId = cookies.userId;
-            // connect to database
+            // connect to database and find user
             await connectToDatabase();
-            const user = await User.findOne({userId}, { projection: { starterIQ: 1, testOneScore: 1, testTwoScore: 1, testThreeScore: 1 } });
+            const user = await User.findOne({userId});
+            console.log("user is ", user);
             if ( !user ) {
                 return res.status(404).json({message: "User not found"})
             }
-            return res.status(200).json({message: "results are here"});
+            // calculate user's iq
+            const rawTotalScore = user.starterIQ + user.testOneScore + user.testTwoScore + user.testThreeScore;
+            console.log(`Raw total score is ${rawTotalScore}/30`);
+            const maxScore = 30; //yes, i know it's 29 but hey only i'm winning here
+            const meanRawScore = 15 //(half of the total availabale score)
+            const sdRawScore = 5 //satndard deviation for raw scores.
+            // convert raw scores into z-scores
+            const zScore = (rawTotalScore - meanRawScore) / sdRawScore;
+            // convert sdscore into raw iq
+            const iq = 100 + zScore * 15;
+            // calculating percentile
+            const calculatePercentile = (iq: number) => {
+                const mean = 100;
+                const standardDeviation = 15;
+                const zScore = ( iq - mean ) / standardDeviation;
+                const percentile = ( 1 + erf(zScore/Math.sqrt(2))) / 2 * 100;
+                console.log("The percentile is", percentile)
+                return percentile;
+            }
+            // calculating deviation
+            const calculateDeviation = (iq: number) => {
+                const mean = 100;
+                const standardDeviation = 15;
+                const deviation = ( iq - mean )/standardDeviation;
+                console.log("The deviation is", deviation)
+                return deviation;
+            }
+            console.log(iq);
+            const percentile = calculatePercentile(iq)
+            const deviation = calculateDeviation(iq);
+            return res.status(200).json({iq: iq, deviation: deviation, percentile: percentile});
         }
         catch ( err ) {
             console.log("error when fetching user's results", err);

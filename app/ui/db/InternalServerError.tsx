41869e9ec9b6c5b293008d489eac9d9163d73366
Jaarabytes <xavierandole78@gmail.com>
2024-06-14 17:35:58 +0300

    style: add relevant tailwindcss classes
    Add relevant tailwindcss classes for styling of components. next is to
    integrate them into the app

--- app/ui/db/InternalServerError.tsx
+++ app/ui/db/InternalServerError.tsx
@@ -1,5 +1,14 @@
 
 
 export default function InternalServerError() {
-
+	return (
+		<>
+			<div>
+				<h1 className="my-5 text-2xl font-bold">Internal server error</h1>
+				<p className="my-5">Sorry, our server encountered some errors while processing your request</p>
+				<hr />
+				<button className="px-3 py-2 my-5 bg-green-500 hover:bg-green-400">Start over ? </button>
+			</div>
+		</>
+	)
 }
--- app/ui/db/UserNotFound.tsx
+++ app/ui/db/UserNotFound.tsx
@@ -1,5 +1,13 @@
 
 
 export default function UserNotFound () {
-    
+    return (
+        <>
+            <div className="text-center" style={{minHeight: "100vh"}}>
+                <h1 className="font-bold text-2xl">User not found</h1>
+                <p className="">Sorry, it seems that your results were not found in our database</p>
+                <button className="px-3 py-5 rounded-lg bg-green-500 hover:bg-green-400">Re-do the test ?</button>
+            </div>
+        </>
+    )
 }

924325518979977e2e7a2e32004f2cdb0631ec3e
Jaarabytes <xavierandole78@gmail.com>
2024-06-26 11:55:54 +0300

    feat: add mongodb integration
    integrate mongodb to store user's score

---
+++ pages/api/test-three.ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase, User } from "./connect";
+import { parseCookies } from "./cookieParser";
+import { Cookies } from "./cookieParser";
+// computation for test three of the exams
+// Critical Thinking is part three of the test
+export default async function handler (req: NextApiRequest, res:NextApiResponse ){
+    if ( req.method == "POST" ) {
+        // connect to database
+        console.log("Connecting to database")
+        await connectToDatabase();
+        // get the userId/ cookie value
+        console.log("Getting cookies")
+        const cookies: Cookies = parseCookies(req.headers);
+        const userId = cookies.userId;
+        console.log("The userId (cookie) is ", userId);
+        let testThreeScore = 0;
+        console.log("The request body is", req.body);
+        const userChoices = JSON.parse(req.body);
+        console.log("Values equals", userChoices);
+        // create an array of answers and countercheck with values provided by user
+        const answers = ["fleeting", "artist", "magnanimous", "sculpture", "True", "Box that has misfortunes and hope trapped inside",
+         "False", "Cultural trend", "Economy", "argue with the opposite"];
+        // initialize types
+        type userChoices = {[key: string]: string};
+        type answers = string[]
+        // function to countercheck
+        const checkValidity = ( userChoices: userChoices, answers: answers ) => {
+            const values = Object.values(userChoices)
+            // console.log("Mapped into, ",values);
+            for (let i = 0; i < values.length; i++ ){
+                if (values[i] == answers[i]) {
+                    testThreeScore++
+                }
+            }
+            console.log(testThreeScore);
+            return testThreeScore;
+        }
+        checkValidity(userChoices, answers);
+        const response = { testThreeScore }
+        await User.updateOne({ userId }, { $set: { testThreeScore: response.testThreeScore } }, { new: true });
+        return res.status(200).json({response});
+    }
+    else {
+        res.setHeader('Allow', ["POST"]);
+        res.status(405).end(`Method ${req.method} not allowed`)
+    }
+}

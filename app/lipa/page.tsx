b2ac6449c8d88e18baa330ce8197e5cea174b356
Jaarabytes <xavierandole78@gmail.com>
2024-06-29 12:23:26 +0300

    fix: implement paystack the right way
    Implement paystack the right way which works. I can now receive payments
    through my app, but to open up and USD account would be 100x better.
    also remove stupid daraja api

--- app/lipa/page.tsx
+++ app/lipa/page.tsx
@@ -1,27 +1,34 @@
-import Link from "next/link"
-import { MdKeyboardArrowUp } from "react-icons/md"
-import Image from "next/image"
-export default function Lipa () {
+'use client'
+// Paystack component that accepts donations
+//
+import { usePaystackPayment, PaystackButton } from 'react-paystack';
+import { useState } from 'react';
+export default function Donate () {
+    const [ amount, setAmount ] = useState(0);
+    const config = {
+        reference: (new Date()).getTime().toString(),
+        email: "xh3rking96@gmail.com", 
+        amount: amount * 100,
+        publicKey: 'pk_live_9999378c83331abc0c642f96d2457a5f88969934',
+        currency: 'KES'
+        }
+    const onSuccess = ( reference: any ) => { console.log(reference) }
+    const onClose = () => { console.log("closed") }
     return (
-        <>
-            <div className="px-5 text-center" style={{minHeight: "100vh"}}>
-                <h1 className="font-bold text-3xl my-10">which way?</h1>
-                {methods.map((element, index) => (
-                    <div key={index} className="flex justify-center sm:inline">
-                        <Link href={`/lipa${element.link}`}>
-                            <button className={`rounded-lg px-5 py-3 bg-green-900 
-                            px-5 text-white m-5 ${element.name == "M-pesa" ? 
-                            "bg-green-900 hover:bg-green-700" : "bg-violet-900 hover:bg-violet-700"}`}><MdKeyboardArrowUp 
-                            className="inline"/> {element.name} <MdKeyboardArrowUp className="inline"/></button>
-                        </Link>
-                    </div>
-                ))}
-                <div className="flex justify-center">
-                    <Image className="my-5" src="/for_loyal_customer.png" height={200} width={200} alt="Please make a choice" />
+            <>
+                <div className='text-center my-5' style={{minHeight: '100vh'}}>  
+                    <h1 className='text-2xl font-bold my-5'>Thank you for donating anon!</h1>
+                    <label>Amount (in KES)</label>
+                    <br /><br />
+                    <input className='text-center rounded-sm text-black py-3' value={amount} onChange={(e) => setAmount(Number(e.target.value))} />
+                    <br /><br />
+                    <PaystackButton
+                    className='my-5 py-2 text-white rounded-lg px-3 bg-green-900 hover:bg-green-700'
+                    onSuccess={onSuccess} onClose={onClose}
+                    text='Money Upvote'
+                    {...config}
+                    />
                 </div>
-            </div>
-        </>
-    )
+           </>
+           )
 }
-// add donorbox to donate
-const methods = [{name: "Donate", link: "/donate"}, {name: "M-pesa", link: '/mpesa'}]

329cde95cfcd31902b077f396dfa5186e853872b
Jaarabytes <xavierandole78@gmail.com>
2024-07-15 15:44:34 +0300

    fix: removed that stupid font
    Stupid font goodbye

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import TestLayout from "./layout/TestLayout";
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "IQ test by Jaarabytes",
@@ -16,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}><TestLayout>{children}</TestLayout></body>
+      <body><TestLayout>{children}</TestLayout></body>
     </html>
   );
 }

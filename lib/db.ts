c5fea74b53cd8a1ee4b51ff6ed909107dec86494
Jaarabytes <xavierandole78@gmail.com>
2024-07-15 15:43:05 +0300

    feat: db connections and others
    File that connects to the mongo db

---
+++ lib/db.ts
@@ -0,0 +1,59 @@
+// Connects to mongo and everything mongo
+import mongoose, { Schema , model, models } from "mongoose";
+
+const uri: string = process.env.MONGO_URI as string;
+
+interface IUser {
+    userId?: String,
+    starterIQ?: Number,
+    testOneScore?: Number,
+    testTwoScore?: Number,
+    testThreeScore?: Number,    
+} 
+
+// set up a mongoose schema
+const userSchema = new Schema<IUser>({
+    userId : {
+        type: String,
+        unique: true
+    },
+    starterIQ : {
+        type: Number,
+        default: 0
+    },
+    testOneScore: {
+        type: Number,
+        default: 0
+    },
+    testTwoScore: {
+        type: Number,
+        default: 0
+    },
+    testThreeScore: {
+        type: Number,
+        default: 0
+    },
+})
+
+const User = models.User || model<IUser>('User', userSchema);
+
+export async function connectToDatabase () {
+    if ( mongoose.connection.readyState >= 1 ) {
+        return;
+    }
+    return mongoose.connect( uri )
+}
+
+// Set up a connection to the mongodb database
+export default async function handler () {
+    if ( !uri ) {
+        console.log("Mongo db uri doesn't exist");
+    }
+    try {
+        await connectToDatabase();
+    }
+    catch ( err ) {
+        console.error("Error when connecting to mongodb", err);
+    }
+}
+export { User }

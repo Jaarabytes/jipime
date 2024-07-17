c712d14718b4a76c68353652c698b7374bf6056f
Jaarabytes <xavierandole78@gmail.com>
2024-07-17 17:15:03 +0300

    style: add relevant syles
    Add styles such a sminiumu height and centering for the error
    components, otherwise the app works perfectly

--- app/ui/db/InternalServerError.tsx
+++ app/ui/db/InternalServerError.tsx
@@ -1,12 +1,12 @@
 export default function InternalServerError() {
 	return (
-		<>
-			<div>
+		<main style={{ minHeight: '100vh' }}>
+			<div className="text-center">
 				<h1 className="my-5 text-2xl font-bold">Internal server error</h1>
 				<p className="my-5">Sorry, our server encountered some errors while processing your request</p>
 				<hr />
-				<button className="px-3 py-2 my-5 bg-green-500 hover:bg-green-400">Start over ? </button>
+				<a href="https://jipime.vercel.app/"><button className="px-3 py-2 my-5 bg-green-500 hover:bg-green-400">Start over ? </button></a>
 			</div>
-		</>
+		</main>
 	)
 }
--- app/ui/db/UserNotFound.tsx
+++ app/ui/db/UserNotFound.tsx
@@ -4,7 +4,9 @@ export default function UserNotFound () {
             <div className="text-center" style={{minHeight: "100vh"}}>
                 <h1 className="font-bold text-2xl">User not found</h1>
                 <p className="">Sorry, it seems that your results were not found in our database</p>
-                <button className="px-3 py-5 rounded-lg bg-green-500 hover:bg-green-400">Re-do the test ?</button>
+                <a href="https://jipime.vercel.app">
+                  <button className="px-3 py-5 rounded-lg bg-green-500 hover:bg-green-400">Re-do the test ?</button>
+                </a>
             </div>
         </>
     )

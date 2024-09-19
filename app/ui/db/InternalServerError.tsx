f8afa650724e38144cc7cbdd9d6fbaa84fb95e21
jaarabytes <xavierandole78@gmail.com>
2024-09-19 14:43:12 +0300

    fix CI?

--- app/ui/db/InternalServerError.tsx
+++ app/ui/db/InternalServerError.tsx
@@ -5,7 +5,7 @@ export default function InternalServerError() {
 				<h1 className="my-5 text-2xl font-bold">Internal server error</h1>
 				<p className="my-5">Sorry, our server encountered some errors while processing your request</p>
 				<hr />
-				<a href=""/"><button className="px-3 py-2 my-5 bg-green-700 rounded-lg text-white hover:bg-green-600">Start over ? </button></a>
+				<a href="/"><button className="px-3 py-2 my-5 bg-green-700 rounded-lg text-white hover:bg-green-600">Start over ? </button></a>
 			</div>
 		</main>
 	)
